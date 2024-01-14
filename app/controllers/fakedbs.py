c5a9d38009419b6f985840e7ccf951348ecf1a05
Amy Sandar Uccello <auccell@uwo.ca>
2024-01-14 04:24:07 -0500

    post operations working on local memory

--- app/controllers/fakedbs.py
+++ app/controllers/fakedbs.py
@@ -3,3 +3,8 @@ fakeDatabase = {
     2:{'name':'Christine', 'email':'christine@email.com', 'password':'5678', 'achieveVal':'10', 'username':'bristcautista', 'picture':'/static/img/christ.png'},
     3:{'name':'Golshan', 'email':'golshan@email.com', 'password':'8912', 'achieveVal':'100', 'username':'golshan', 'picture':'/static/img/golshan.png'},
 }
+
+fakePostTable = { 
+    1:{'userid':1, 'title':'title01', 'attachment':'.mp4', 'description':'description01', 'tag':'tag01', 'date':'date01'},
+}
+
